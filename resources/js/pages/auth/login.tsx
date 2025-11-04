78e3b713f9cd71e767f0fba9a215ea430312612e
Alberto Murcia <luismurciacasfid.com>
2025-11-04 15:33:18 +0100

    Solucionado bug de rutas

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -34,7 +34,7 @@ export default function Login({
             <Head title="Log in" />
 
             <Form
-                {...store.form()}
+                {...store.post()}
                 resetOnSuccess={['password']}
                 className="flex flex-col gap-6"
             >
